58e39b48e44d22320d669163a033f3a24a05b9e2
AdrianBerardo <adrianberardomatias@gmail.com>
2022-10-13 21:31:23 -0300

    Clase 4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.sdfsdfsdf
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactg
-        </a>
-      </header>
-    </div>
+    <>
+
+    <Header
+      NombrePagina="Pagina Web"
+      A
+    />
+    <Footer/>
+
+    </>
   );
 }
 
